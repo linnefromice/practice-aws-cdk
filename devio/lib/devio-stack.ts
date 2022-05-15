47dfa0bc46aa511e9af8dec78ead4f451fae2d2f
linnefromice <linnefromice@gmail.com>
2022-05-16 01:05:41 +0900

    feat: create Vpc

--- devio/lib/devio-stack.ts
+++ devio/lib/devio-stack.ts
@@ -1,9 +1,10 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Vpc } from '@aws-cdk/aws-ec2';
+import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class DevioStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class DevioStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
@@ -12,5 +13,7 @@ export class DevioStack extends Stack {
     // const queue = new sqs.Queue(this, 'DevioQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new Vpc(this, "Vpc");
   }
 }
