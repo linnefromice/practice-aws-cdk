2995943fba8341be794ca22dc8cf5097345bb1a8
linnefromice <linnefromice@gmail.com>
2022-05-16 22:47:16 +0900

    feat(#3): test

--- devio/test/devio.test.ts
+++ devio/test/devio.test.ts
@@ -1,10 +1,12 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as Devio from '../lib/devio-stack';
+import * as cdk from '@aws-cdk/core';
+import * as Devio from '../lib/devio-stack';
+import { expect, countResources, haveResource } from '@aws-cdk/assert';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/devio-stack.ts
-test('SQS Queue Created', () => {
+// test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
 //   const stack = new Devio.DevioStack(app, 'MyTestStack');
@@ -14,4 +16,15 @@ test('SQS Queue Created', () => {
 //   template.hasResourceProperties('AWS::SQS::Queue', {
 //     VisibilityTimeout: 300
 //   });
-});
+// });
+
+test("Vpc", () => {
+  const app = new cdk.App();
+  const stack = new Devio.DevioStack(app, "DevioStack");
+
+  expect(stack).to(countResources("AWS::EC2::VPC", 1))
+  expect(stack).to(haveResource("AWS::EC2::VPC", {
+    CidrBlock: "10.0.0.0/16",
+    Tags: [{ "Key": "Name", "Value": "devio-stg-vpc" }]
+  }))
+})
