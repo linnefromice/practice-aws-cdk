c5ed20bbda79c9622484a3f3199a4370ecf6bcb9
linnefromice <linnefromice@gmail.com>
2022-05-17 12:04:49 +0900

    feat(#7): refactor - separate files

---
+++ devio/lib/vpc.ts
@@ -0,0 +1,19 @@
+import * as cdk from "@aws-cdk/core";
+import { CfnVPC } from "@aws-cdk/aws-ec2";
+
+
+export class Vpc {
+  public vpc: CfnVPC;
+
+  constructor() {};
+
+  public createResouces(scope: cdk.Construct) {
+    const systemName = scope.node.tryGetContext("systemName")
+    const envType = scope.node.tryGetContext("envType")
+
+    this.vpc = new CfnVPC(scope, "Vpc", {
+      cidrBlock: "10.0.0.0/16",
+      tags: [{ key: "Name", value: `${systemName}-${envType}-vpc`}]
+    })
+  }
+}
