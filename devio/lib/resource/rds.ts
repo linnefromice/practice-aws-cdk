f9c8d9e77e419a128970a2b4c9896ee081abfa70
linnefromice <linnefromice@gmail.com>
2022-05-23 12:57:54 +0900

    feat(#22): rds cluster

--- devio/lib/resource/rds.ts
+++ devio/lib/resource/rds.ts
@@ -1,25 +1,38 @@
 import * as cdk from '@aws-cdk/core';
-import { CfnDBClusterParameterGroup, CfnDBParameterGroup, CfnDBSubnetGroup } from '@aws-cdk/aws-rds';
-import { CfnSubnet } from '@aws-cdk/aws-ec2';
+import { CfnDBCluster, CfnDBClusterParameterGroup, CfnDBParameterGroup, CfnDBSubnetGroup } from '@aws-cdk/aws-rds';
+import { CfnSecurityGroup, CfnSubnet } from '@aws-cdk/aws-ec2';
 import { Resource } from './abstract/resource';
+import { CfnSecret } from '@aws-cdk/aws-secretsmanager';
+import { OSecretKey, SecretsManager } from './secretsManager';
 
 export class Rds extends Resource {
+  public dbCluster: CfnDBCluster
+
   private readonly subnetDb1a: CfnSubnet
   private readonly subnetDb1c: CfnSubnet
+  private readonly securityGroupRds: CfnSecurityGroup
+  private readonly secretRdsCluster: CfnSecret
+
+  private static readonly databaseName = "devio"
 
   constructor(
     subnetDb1a: CfnSubnet,
     subnetDb1c: CfnSubnet,
+    securityGroupRds: CfnSecurityGroup,
+    secretRdsCluster: CfnSecret
   ) {
     super();
     this.subnetDb1a = subnetDb1a;
     this.subnetDb1c = subnetDb1c;
+    this.securityGroupRds = securityGroupRds;
+    this.secretRdsCluster = secretRdsCluster;
   };
 
   createResources(scope: cdk.Construct): void {
-    this.createSubnetGroup(scope)
-    this.createClusterParameterGroup(scope)
+    const subnetGroup = this.createSubnetGroup(scope)
+    const clusterParameterGroup = this.createClusterParameterGroup(scope)
     this.createParameterGroup(scope)
+    this.dbCluster = this.createCluster(scope, subnetGroup, clusterParameterGroup)
   }
 
   private createSubnetGroup(scope: cdk.Construct): CfnDBSubnetGroup {
@@ -42,6 +55,27 @@ export class Rds extends Resource {
     return new CfnDBParameterGroup(scope, "ParameterGroupRds", {
       description: "Parameter Group for RDS",
       family: "aurora-mysql5.7",
+    })
+  }
+
+  private createCluster(scope: cdk.Construct, subnetGroup: CfnDBSubnetGroup, clusterParameterGroup: CfnDBClusterParameterGroup): CfnDBCluster {
+    return new CfnDBCluster(scope, "RdsDbCluster", {
+      engine: "aurora-mysql",
+      backupRetentionPeriod: 7,
+      databaseName: Rds.databaseName,
+      dbClusterIdentifier: this.createResourceName(scope, "rds-cluster"),
+      dbClusterParameterGroupName: clusterParameterGroup.ref,
+      dbSubnetGroupName: subnetGroup.ref,
+      enableCloudwatchLogsExports: ["error"],
+      engineMode: "provisioned",
+      engineVersion: "5.7.mysql_aurora.2.10.0",
+      masterUserPassword: SecretsManager.getDynamicReference(this.secretRdsCluster, OSecretKey.MasterUserPassword),
+      masterUsername: SecretsManager.getDynamicReference(this.secretRdsCluster, OSecretKey.MasterUsername),
+      port: 3306,
+      preferredBackupWindow: "19:00-19:30",
+      preferredMaintenanceWindow: "sun:19:30-sun:20:00",
+      storageEncrypted: true,
+      vpcSecurityGroupIds: [this.securityGroupRds.attrGroupId]
     })
   }
 }
