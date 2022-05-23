d2d75354b6535ffbadf590b3c683f733ab1a39e4
linnefromice <linnefromice@gmail.com>
2022-05-23 17:03:10 +0900

    feat(#27): init

---
+++ devio-27/test/devio-27.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Devio27 from '../lib/devio-27-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/devio-27-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Devio27.Devio27Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
